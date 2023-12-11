e7853be94c967d2970be3637054933fe618e85a0
vitodursopettersson <vitodurso98@gmail.com>
2023-12-11 13:33:47 +0100

    feat: apphero

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    AppHero,
   }
 };
 
@@ -11,6 +13,7 @@ export default {
 
 <template>
   <AppTitle />
+  <AppHero></AppHero>
 </template>
 
 <style scoped></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+
+}
+
+</script>
+
+<template>
+    <img src="../assets/vite-logo.png" alt="vite-logo">
+    <img src="../assets/vue-logo.png" alt="vite-logo">
+</template>
+
+<style scoped>
+img {
+    width: 100px;
+    margin: 10px;
+}
+</style>
